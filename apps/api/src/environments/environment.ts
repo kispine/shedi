eeca11e0bc8c65a79552119fe3ecd631e165f85a
kikspine <kikspine@gmail.com>
2022-02-01 13:51:00 +0400

    feat(db): Add db
    * add typeorm
    * add scripts
    
    Closes #3

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,23 @@
 export const environment = {
   production: false,
+  connection: {
+    type: process.env.DB_TYPE as 'aurora-data-api',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_USER_PASSWORD,
+    database: process.env.DB_NAME,
+    dropSchema: false,
+    synchronize: true,
+    logging: true,
+    entities: [
+      'dist/app/api/src/**/*.entity[.ts, .js]'
+    ],
+    migrations: [
+      'dist/app/api/migrations/*[.ts, .js]'
+    ],
+    cli: {
+      'migrationsDir': 'db/migrations'
+    },
+  }
 }
