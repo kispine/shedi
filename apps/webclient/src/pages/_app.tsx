0c2ece9a0e904de1b7b28a3bcf86e97fb2384a5b
kikspine <kikspine@gmail.com>
2022-02-01 16:17:41 +0400

    feat(scss): add scss
    * add scss dependencies
    * add styles structure
    
    Closes #8

--- apps/webclient/src/pages/_app.tsx
+++ apps/webclient/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import './styles.css'
+import '../styles/main.scss'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,9 +8,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to webclient!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   )
 }
---
+++ apps/webclient/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import classes from './index.module.scss'
+
+export function Index() {
+  return (
+    <div className={classes.demo}>index page</div>
+  )
+}
+
+export default Index
